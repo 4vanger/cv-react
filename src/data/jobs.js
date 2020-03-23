4ed11a849e049894d6029e53015d30651494327a
Andrew Kulinich <github@4vanger.com>
2020-03-23 16:06:07 -0700

    add bootstrap and SASS strip out create-react-app stuff

---
+++ src/data/jobs.js
@@ -0,0 +1,187 @@
+[
+    {
+    "id": "itgrp",
+    "company": "IT Group",
+    "href": "http://www.itgrp.net/",
+    "position": "Web developer, project manager",
+    "notes": "My first job, using Ruby, first projects with JS. Acting as a project manager.",
+    "skills": {
+    "technical": [
+    "ruby",
+    "html",
+    "javascript",
+    "postgresql"
+    ],
+    "managerial": [
+    "project management"
+    ]
+    },
+    "start": "1 Feb 2003",
+    "finish": "1 Oct 2004"
+    },
+    {
+    "id": "uwiss",
+    "company": "U-wiss",
+    "href": "http://www.u-wiss.com/",
+    "position": "Java/Javascript web developer",
+    "notes": "Started as a Java developer - building all that huge ERP, B2B systems using JBoss, EJB etc. Then switched to writing Javascript and stucked there for next 6 years (at present moment)",
+    "skills": {
+    "technical": [
+    "java",
+    "j2ee",
+    "jsp",
+    "postgresql",
+    "sql",
+    "javascript"
+    ]
+    },
+    "start": "1 Oct 2004",
+    "finish": "1 May 2006"
+    },
+    {
+    "id": "rac",
+    "href": "http://www.rentacoder.com/",
+    "position": "Freelance developer",
+    "notes": "Great way to learn something new. Different tasks every day, working at night to get into customer timezone. Selling Javascript as my major skill.",
+    "skills": {
+    "technical": [
+    "javascript",
+    "html",
+    "css"
+    ],
+    "managerial": [
+    "lack of sleep",
+    "selling myself"
+    ]
+    },
+    "start": "1 Jun 2005",
+    "finish": "1 May 2006"
+    },
+    {
+    "id": "zapatec",
+    "company": "Zapatec",
+    "href": "http://www.zapatec.com/",
+    "position": "AJAX tech lead, department manager",
+    "notes": "Developing Zapatec AJAX Suite - commercial Javascript framework with very handy widgets. Opened an R&D office in Chernivtsi, Ukraine. Growing the team up to 12 developers.",
+    "skills": {
+    "technical": [
+    "javascript",
+    "html",
+    "css",
+    "dhtml",
+    "web2.0"
+    ],
+    "managerial": [
+    "office work administration",
+    "team leading",
+    "team building",
+    "project management"
+    ]
+    },
+    "start": "1 May 2006",
+    "finish": "1 Nov 2008"
+    },
+    {
+    "id": "funambol",
+    "company": "Funambol",
+    "href": "http://www.funambol.com/",
+    "position": "AJAX tech lead, department manager",
+    "notes": "Funambol acquired Zapatec and we concentrated on building solid web 2.0 app for working with phone sync data. <a href='http://my.funambol.com/' target='_blank'>Try it live</a>. Still managing ukrainian R&D office.",
+    "skills": {
+    "technical": [
+    "javascript",
+    "mobile",
+    "web2.0",
+    "html",
+    "css",
+    "ux"
+    ],
+    "managerial": [
+    "office work administration",
+    "team leading",
+    "team building"
+    ]
+    },
+    "start": "1 Nov 2008",
+    "finish": "1 Jan 2011"
+    },
+    {
+    "id": "appcelerator",
+    "company": "Appcelerator",
+    "href": "http://www.appcelerator.com/",
+    "position": "AJAX tech lead, department manager",
+    "notes": "After Funambol my team started working for Appcelerator. We've created environment for creating, managing and running black-box tests for mobile applications. Also in a very short term developed <a href='http://developer.appcelerator.com/blog/2011/05/major-titanium-updates.html' target='_blank'>HTML5 Mobile Web SDK</a> for running your Titanium projects inside browser.",
+    "skills": {
+    "technical": [
+    "javascript",
+    "mobile",
+    "html5",
+    "ux"
+    ],
+    "managerial": [
+    "office work administration",
+    "team leading",
+    "team building"
+    ]
+    },
+    "start": "1 Feb 2011",
+    "finish": "1 Jun 2012"
+    },
+    {
+    "id": "milkandhoney",
+    "company": "Milk and Honey Shoes",
+    "href": "http://www.milkandhoneyshoes.com/",
+    "position": "Dev lead",
+    "notes": "Frontend development. Automating data export from Photoshop using PS Scripting",
+    "skills": {
+    "technical": [
+    "html5",
+    "ui",
+    "ux",
+    "photoshop scripting",
+    "prototype.js"
+    ]
+    },
+    "start": "1 Jul 2012",
+    "finish": "1 Nov 2012"
+    },
+    {
+    "id": "zabava",
+    "company": "Zabava",
+    "position": "Developer",
+    "notes": "Developing browser-based slot machine. Designing scalable and easy to extend architecture to develop numerous games on top of it. ",
+    "skills": {
+    "technical": [
+    "html5",
+    "architecture",
+    "canvas",
+    "animations",
+    "web sockets"
+    ]
+    },
+    "start": "1 Sep 2012",
+    "finish": "1 Nov 2012"
+    },
+    {
+    "id": "rr",
+    "company": "RailsReactor",
+    "href": "http://railsreactor.com/",
+    "position": "UI architect",
+    "notes": "Building project architecture for new projects from scratch. Working in a team with backend developers and designers to quickly bring online modern and easy to use web services.",
+    "skills": {
+    "technical": [
+    "html5",
+    "architecture",
+    "d3 framework",
+    "css",
+    "ui",
+    "ux",
+    "backbone.js",
+    "jquery",
+    "html5 apis",
+    "angular.js"
+    ]
+    },
+    "start": "1 Nov 2012"
+    }
+    ]
